1c554509294eba1be1ec51b1daedefa361a132e0
egorpash <2021104302@pnu.edu.ru>
2024-06-29 17:49:20 +0200

    Next commit

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
 import json
 
-
-class Category:
-    all_categories = 0
-
-    def __init__(self, name, description):
-        self._name = name
-        self._description = description
-        self._products = []
-        Category.all_categories += 1
-
-        @property
-        def all_unique_goods(self):
-            return sum(len(set([product.name for product in self._products])) for category in Category.all_categories)
-
-    def add_product(self, product):
-        self._products.append(product)
-
-    def __len__(self):
-        return sum(product.quantity for product in self._products)
-
-    def __str__(self):
-        return f'{self._name}, количество продуктов: {len(self)} шт.'
-
-    @property
-    def products(self):
-        product_list = ""
-        for product in self._products:
-            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
-        return product_list
-
-
 class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
@@ -46,7 +15,10 @@ class Product:
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
     def __add__(self, other):
-        return self.price * self.quantity + other.price * other.quantity
+        if type(self) == type(other):
+            return self.price * self.quantity + other.price * other.quantity
+        else:
+            raise TypeError("Невозможно сложить товары разных типов")
 
     @property
     def price(self):
@@ -58,3 +30,52 @@ class Product:
             print("Цена введена некорректно")
         else:
             self._price = new_price
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, performance, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.performance = performance
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+class Grass(Product):
+    def __init__(self, name, description, price, quantity, country, growth_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.growth_period = growth_period
+        self.color = color
+
+
+class Category:
+    all_categories = 0
+
+    def __init__(self, name, description):
+        self._name = name
+        self._description = description
+        self._products = []
+        Category.all_categories += 1
+
+    @property
+    def all_unique_goods(self):
+        return sum(len(set([product.name for product in self._products])) for category in Category.all_categories)
+
+    def add_product(self, product):
+        if isinstance(product, Product) or issubclass(type(product), Product):
+            self._products.append(product)
+        else:
+            raise TypeError("Невозможно добавить объект, не являющийся продуктом.")
+
+    def __len__(self):
+        return sum(product.quantity for product in self._products)
+
+    def __str__(self):
+        return f'{self._name}, количество продуктов: {len(self)} шт.'
+
+    @property
+    def products(self):
+        product_list = ""
+        for product in self._products:
+            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_list
